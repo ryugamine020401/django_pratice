c49ee0f4f877554baee748cb0bfe91788a6acbf6
YMZK <ryugamine020401@gmail.com>
2023-03-05 22:00:07 +0800

    新增註冊登入功能

--- TEST/TEST/settings.py
+++ TEST/TEST/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'posts.apps.PostsConfig',
     'money.apps.MoneyConfig',
+    'register.apps.RegisterConfig',
 ]
 
 MIDDLEWARE = [
--- TEST/TEST/urls.py
+++ TEST/TEST/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('posts.urls')),
     path('money/', include('money.urls')),
+    path('register/', include('register.urls')),
 ]
